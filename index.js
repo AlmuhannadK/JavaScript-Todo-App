fa4b57f73be61962a8dbe5a16fe808c5ee7ad595
Almuhannad <almuhannad.khalid@gmail.com>
2024-02-21 10:32:50 +0300

    configuring project files

---
+++ index.js
@@ -0,0 +1,38 @@
+/*
+## Level 1: Todo App Basic Requirements
+
+In this level, the application includes the following features:
+
+1. Adding a Todo:
+   - Display an input field where users can enter a new todo item.
+   - Include an "Add" button to add the entered todo item to the list.
+
+2. Rendering Todo List:
+   - Render the list of todos on the screen.
+   - Each todo item should display a checkbox and the task description.
+
+3. Deleting a Todo:
+   - Implement a "Delete" button next to each todo item.
+   - When the button is clicked, remove the corresponding todo item from the list.
+*/
+
+// 1. Adding a Todo:
+//    - Display an input field where users can enter a new todo item.
+//    - Include an "Add" button to add the entered todo item to the list.
+
+const todoList = [];
+
+// function to add elements to todo list
+function addElementToList(arr, element) {
+  return arr.push(element);
+}
+
+// DOM to query select tags (button)
+//link button (event listener) to trigger function when pressed
+
+// 2. Rendering Todo List:
+//    - Render the list of todos on the screen.
+//    - Each todo item should display a checkbox and the task description.
+
+// forEach and dom manipulation to append to template
+//adfasdfasd.addElementToList(kdj, lksjdf);
