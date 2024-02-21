29a798e546c169f93502ef6c9855b14b9491ed67
Almuhannad <almuhannad.khalid@gmail.com>
2024-02-21 17:01:13 +0300

    first commit

--- index.js
+++ index.js
@@ -1,38 +1,14 @@
-/*
-## Level 1: Todo App Basic Requirements
+//let globalToDoList = [];
+const form = document.querySelector(".inner-form");
+const input = document.querySelector(".input-task");
+const ul = document.querySelector("#todos");
 
-In this level, the application includes the following features:
-
-1. Adding a Todo:
-   - Display an input field where users can enter a new todo item.
-   - Include an "Add" button to add the entered todo item to the list.
-
-2. Rendering Todo List:
-   - Render the list of todos on the screen.
-   - Each todo item should display a checkbox and the task description.
-
-3. Deleting a Todo:
-   - Implement a "Delete" button next to each todo item.
-   - When the button is clicked, remove the corresponding todo item from the list.
-*/
-
-// 1. Adding a Todo:
-//    - Display an input field where users can enter a new todo item.
-//    - Include an "Add" button to add the entered todo item to the list.
-
-const todoList = [];
-
-// function to add elements to todo list
-function addElementToList(arr, element) {
-  return arr.push(element);
-}
-
-// DOM to query select tags (button)
-//link button (event listener) to trigger function when pressed
-
-// 2. Rendering Todo List:
-//    - Render the list of todos on the screen.
-//    - Each todo item should display a checkbox and the task description.
-
-// forEach and dom manipulation to append to template
-//adfasdfasd.addElementToList(kdj, lksjdf);
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  let inputValue = input.value;
+  let li = document.createElement("li");
+  li.textContent = inputValue;
+  console.log(li);
+  ul.appendChild(li);
+  form.reset();
+});
