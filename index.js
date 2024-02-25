35ba70dec757f0b1d2cbb2008b4701d1b999d783
Almuhannad <almuhannad.khalid@gmail.com>
2024-02-26 02:21:38 +0300

    added local storage persistence for todo items

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
-let todoItemsList = [];
+let todoItemsList = JSON.parse(localStorage.getItem("todoItems")) || [];
+let editTodoId = -1;
 const form = document.querySelector("#inner-form");
 const input = document.querySelector("#newtodo");
 const todoListElement = document.querySelector(".todos-list");
-let editTodoId = -1;
+const notification = document.querySelector(".notification");
 
+//first render for local storage
+renderTodoList();
+
+//form submission
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
   saveTodo();
   renderTodoList();
+  localStorage.setItem("todoItems", JSON.stringify(todoItemsList));
   form.reset();
 });
 
@@ -19,9 +25,9 @@ function saveTodo() {
     (todo) => todo.value.toUpperCase() === todoValue.toUpperCase()
   );
   if (isEmpty) {
-    alert("input is empty");
+    showNotification("input is empty");
   } else if (isDuplicate) {
-    alert("input already exists");
+    showNotification("Todo already exists");
   } else if (editTodoId >= 0) {
     todoItemsList = todoItemsList.map((todo, index) => {
       return {
@@ -43,8 +49,14 @@ function saveTodo() {
 }
 
 function renderTodoList() {
+  if (todoItemsList.length === 0) {
+    todoListElement.innerHTML = `<p>No tasks available!</p>`;
+    return;
+  }
   //clear before render (otherwise duplicates)
   todoListElement.innerHTML = "";
+
+  //reder todo items
   todoItemsList.forEach((todo, index) => {
     todoListElement.innerHTML += `
     <div class="todo" id=${index}>
@@ -54,7 +66,9 @@ function renderTodoList() {
           style="color : ${todo.color}"
           data-action="check"
           ></i>
-          <p class="checked" data-action="check">${todo.value}</p>
+          <p class="${todo.checked ? "checked" : ""}" data-action="check">${
+      todo.value
+    }</p>
           <i class="fa-solid fa-pen-to-square" data-action="edit"></i>
           <i class="fa-solid fa-trash" data-action="delete"></i>
         </div>
@@ -88,6 +102,7 @@ function checkTodo(todoId) {
     };
   });
   renderTodoList();
+  localStorage.setItem("todoItems", JSON.stringify(todoItemsList));
 }
 
 //edit a todo item
@@ -101,4 +116,15 @@ function deleteTodo(todoId) {
   todoItemsList = todoItemsList.filter((todo, index) => index !== todoId);
   editTodoId = -1;
   renderTodoList();
+  localStorage.setItem("todoItems", JSON.stringify(todoItemsList));
+}
+
+//show notification (styling later)
+function showNotification(msg) {
+  notification.textContent = msg;
+
+  notification.classList.add("notification-enter");
+  setTimeout(() => {
+    notification.classList.remove("notification-enter");
+  }, 3000);
 }
